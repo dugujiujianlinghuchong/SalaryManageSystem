8cf0fb93b00deac9d7ed9e16011bd357a161a208
Yt <yt1160108626@qq.com>
2018-05-29 17:01:15 +0800

    完善了登录页

--- src/App.vue
+++ src/App.vue
@@ -55,7 +55,19 @@
 
     <!-- 登录页 -->
     <div v-else>
-      <el-button>Start</el-button>
+      <el-form ref="AccountFrom" :model="account" :rules="rules" label-position="left" label-width="0px" class="demo-ruleForm login-container">
+        <h3 class="title">管理员登录</h3>
+        <el-form-item prop="strUserName">
+          <el-input type="text" v-model="account.strUserName" auto-complete="off" placeholder="账号"></el-input>
+        </el-form-item>
+        <el-form-item prop="strPSD">
+          <el-input type="password" v-model="account.strPSD" auto-complete="off" placeholder="密码"></el-input>
+        </el-form-item>
+        <!-- <el-checkbox v-model="checked" checked class="remember">记住密码</el-checkbox> -->
+        <el-form-item style="width:100%;">
+          <el-button type="primary" style="width:100%;" @click.native.prevent="handleLogin" :loading="loading">登录</el-button>
+        </el-form-item>
+      </el-form>
     </div>
     <!-- 登录页 end -->
 
@@ -71,7 +83,25 @@ import StaffManage from "./components/staffManage";
 export default {
   data() {
     return {
-      yhbh: "", //用户编号
+      // 以下是登录页数据
+      loading: false,
+      account: {
+        strUserName: "",
+        strPSD: ""
+      },
+      rules: {
+        username: [
+          { required: true, message: "请输入账号", trigger: "blur" }
+          //{ validator: validaePass }
+        ],
+        pwd: [
+          { required: true, message: "请输入密码", trigger: "blur" }
+          //{ validator: validaePass2 }
+        ]
+      },
+      checked: true,
+      // 以下是主页数据
+      yhbh: "",
       dlm: "",
       editableTabsValue: "0",
       editableTabs: [
@@ -92,14 +122,19 @@ export default {
     StaffManage
   },
   methods: {
-    validateUserInfo() {
+    handleLogin() {
       var vueThis = this;
       vueThis.$http
         .get(
           "http://localhost/Gateway4CWGL/MinaMap_UserService.svc/ValidateUserInfo",
-          { params: { strUserName: "wx", strPSD: "1" } }
+          { params: vueThis.account }
         )
         .then(function(response) {
+          if (response.data == "") {
+            vueThis.$alert("用户名或密码错误，请检查后重试！", "提示", {
+              confirmButtonText: "确定"
+            });
+          }
           vueThis.yhbh = response.data.YHBH;
           vueThis.dlm = response.data.XTDLM;
 
@@ -110,6 +145,24 @@ export default {
           vueThis.$store.commit("getSystemInfo", systemInfo);
         });
     },
+    // validateUserInfo() {
+    //   var vueThis = this;
+    //   vueThis.$http
+    //     .get(
+    //       "http://localhost/Gateway4CWGL/MinaMap_UserService.svc/ValidateUserInfo",
+    //       { params: vueThis.account }
+    //     )
+    //     .then(function(response) {
+    //       vueThis.yhbh = response.data.YHBH;
+    //       vueThis.dlm = response.data.XTDLM;
+
+    //       // 将系统信息写入Vuex
+    //       var systemInfo = {};
+    //       systemInfo.yhbh = vueThis.yhbh;
+    //       systemInfo.screenWidth = document.body.offsetWidth;
+    //       vueThis.$store.commit("getSystemInfo", systemInfo);
+    //     });
+    // },
     addTab(targetName, title, content, closable) {
       for (var i = 0; i < this.editableTabs.length; i++) {
         console.log(this.editableTabs[i]);
@@ -154,7 +207,7 @@ export default {
       // return;
     }
 
-    this.validateUserInfo();
+    // this.validateUserInfo();
   }
 };
 </script>
@@ -231,5 +284,48 @@ export default {
 .grid-content {
   border-radius: 4px;
   min-height: 36px;
+}
+
+// 以下是登录页css
+// body {
+//   background: #dfe9fb;
+// }
+.login-container {
+  /*box-shadow: 0 0px 8px 0 rgba(0, 0, 0, 0.06), 0 1px 0px 0 rgba(0, 0, 0, 0.02);*/
+  -webkit-border-radius: 5px;
+  border-radius: 5px;
+  -moz-border-radius: 5px;
+  background-clip: padding-box;
+  margin: 260px auto;
+  width: 350px;
+  padding: 35px 35px 15px 35px;
+  background: #fff;
+  border: 1px solid #eaeaea;
+  box-shadow: 0 0 25px #cac6c6;
+
+  background: -ms-linear-gradient(top, #ace, #00c1de); /* IE 10 */
+  background: -moz-linear-gradient(top, #ace, #00c1de); /*火狐*/
+  background: -webkit-gradient(
+    linear,
+    0% 0%,
+    0% 100%,
+    from(#ace),
+    to(#00c1de)
+  ); /*谷歌*/
+  background: -webkit-linear-gradient(
+    top,
+    #ace,
+    #00c1de
+  ); /*Safari5.1 Chrome 10+*/
+  background: -o-linear-gradient(top, #ace, #00c1de); /*Opera 11.10+*/
+
+  .title {
+    margin: 0px auto 40px auto;
+    text-align: center;
+    color: #505458;
+  }
+  .remember {
+    margin: 0px 0px 35px 0px;
+  }
 }
 </style>
